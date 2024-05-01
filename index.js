02b8c89dfde0fa797de0a8d347c7d75585195dad
Santo1997 <hossainsanto2@gmail.com>
2024-05-01 18:21:36 +0600

    convert sqlite to mongodb

--- index.js
+++ index.js
@@ -1,52 +1,57 @@
-const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
-const cors = require("cors");
+const {MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
+require("dotenv").config();
+var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const db = new sqlite3.Database("./hadith_db.db", sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Connected to the in-memory SQlite database.");
-});
-
 app.use(cors());
+app.use(express.json());
 
-app.get("/getBooks", async (req, res) => {
-  let sql = "SELECT * FROM 'books'";
-  db.all(sql, (err, result) => {
-    if (err) return console.error(err.message);
-    res.send(result);
-  });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qpcvbrd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.get("/getChapter", async (req, res) => {
-  let sql = "SELECT * FROM 'chapter'";
-  db.all(sql, (err, result) => {
-    if (err) return console.error(err.message);
-    res.send(result);
-  });
-});
+async function run() {
+  try {
+    const books = client.db("idr").collection("books");
+    const chapter = client.db("idr").collection("chapter");
+    const hadith = client.db("idr").collection("hadith");
+    const section = client.db("idr").collection("section");
 
-app.get("/getHadith", async (req, res) => {
-  let sql = "SELECT * FROM 'hadith'";
-  db.all(sql, (err, result) => {
-    if (err) return console.error(err.message);
-    res.send(result);
-  });
-});
+    app.get("/getBooks", async (req, res) => {
+      const result = await books.find().toArray();
+      res.send(result);
+    });
+    app.get("/getChapter", async (req, res) => {
+      const result = await chapter.find().toArray();
+      res.send(result);
+    });
+    app.get("/getHadith", async (req, res) => {
+      const result = await hadith.find().toArray();
+      res.send(result);
+    });
+    app.get("/getSection", async (req, res) => {
+      const result = await section.find().toArray();
+      res.send(result);
+    });
 
-app.get("/getSection", async (req, res) => {
-  let sql = "SELECT * FROM 'section'";
-  db.all(sql, (err, result) => {
-    if (err) return console.error(err.message);
-    res.send(result);
-  });
-});
+    await client.db("admin").command({ping: 1});
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello SQLite");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
